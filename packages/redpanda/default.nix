52d717786845ad30fc4a3793331e718a14d294f5
Taeer Bar-Yam <taeer.bar-yam@tweag.io>
2023-12-19 12:34:25 -0500

    update to 23.2.19

--- packages/redpanda/default.nix
+++ packages/redpanda/default.nix
@@ -3,14 +3,14 @@
 let
   arch = if stdenv.isAarch64 then "arm" else "amd";
   sha256s = {
-    amd = "sha256-Zk9UOGyuIHhFBT01BjCpkyrI6ZZU+SxCtxBdLiuj5Ds=";
+    amd = "sha256-Y6R6312zD0bkAxnDIEH2czJF4Utv4lRKIzoPyDdfCdM=";
     arm = ""; # TODO: Figure out this
   };
 
 in
 stdenv.mkDerivation rec {
   pname = "redpanda-bin";
-  version = "23.2.17";
+  version = "23.2.19";
 
   src = fetchzip {
     url = "https://dl.redpanda.com/nzc4ZYQK3WRGd9sy/redpanda/raw/names/redpanda-${arch}64/versions/${version}/redpanda-${version}-${arch}64.tar.gz";
